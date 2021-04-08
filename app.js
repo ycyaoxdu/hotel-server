b1f36cb86a0093d8c064f24e43eac0229b32cb8c
YYC0626 <yaoyuchen0626@163.com>
2021-04-08 10:42:46 +0800

    mod

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
+//引入
 const Koa = require('koa');
+const Router = require('koa-router');
+
+
+//实例化
 const app = new Koa();
+const router = new Router();
+
 
 //配置路由
+router.get('/', async (ctx)=>{
+
+  ctx.body = 'index';   //返回数据
+
+}).get('/new', async (ctx)=>{
+
+  console.log(ctx.query);
+  console.log(ctx.querystring);
+  console.log(url);
+
+  ctx.body = 'new';
+
+}).get('/newscontent/:aid', async (ctx)=>{    //获取动态路由
+  console.log(ctx.params);
+  ctx.body='test';
+})
+
+
+//
+
+//启动路由
+app
+  .use(router.routes())               /* 启动路由 */
+  .use(router.allowedMethods());      /* 自动设置响应头 */
+
+//
+
 
 //中间件
 
+//
 
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
+
+
+//启动监听，port 3000
 app.listen(3000);
 
