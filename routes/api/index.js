023e7035321c8b560fbdc2e726e5db775e9f401e
YYC0626 <yaoyuchen0626@163.com>
2021-04-15 14:41:44 +0800

    a

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,7 +9,7 @@ const db = require('../../module/db');
 //接收code--》请求接口--》记录openid
 
 let appid = "wx3590eb11fdf22ed3";
-let secret = "91cafdbbc078432640474f36c783b7e0";
+let secret = "";
 let grant_type ="authorization_code";
 
 
@@ -18,11 +18,11 @@ router.get('/', async (ctx)=>{
 
   //console.log(ctx.request.query.code);
   const info = await request.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${ctx.request.query.code}&grant_type=${grant_type}`);
-  ctx.body = "login success";
+  ctx.body = info;
   userinfo = JSON.parse(info);
 
   let result = await db.find('userinfo',  {"openid":userinfo.openid});
-  console.log(result);
+  //console.log(result);
   if(result.length > 0) return;
   let data = await db.insert('userinfo', userinfo);
 })
--- routes/api/userinfo.js
+++ routes/api/userinfo.js
@@ -10,12 +10,42 @@ router.post('/', async (ctx)=>{
 
   ctx.body = 'user info api'
   //await console.log(ctx)
-  //await console.log(ctx.request)
+  await console.log(ctx.request)
   await console.log(ctx.request.body)
 
 })
 
+//信息修改
+router.post('/edit', async (ctx)=>{
 
+  //  let data = await db.update('user', {"username":"zhangsan"}, {"password":"abcdef"});
+  //  console.log(data);
+  
+    //通过id获取用户
+    let id = ctx.request.body.openid;
+    let data = await db.find('userinfo', {"openid": id });
+
+    ctx.body = data ;
+  
+  
+  })
+  
+  //
+  router.post('/doEdit', async (ctx)=>{
+  
+    var id = ctx.request.body.openid;
+    var name = ctx.request.body.name;
+    var phonenumber = ctx.request.body.phonenumber;
+  
+    let data = await db.update('userinfo', {"openid":id}, {
+      name, phonenumber
+    } )
+    
+    ctx.body = 'edit success';
+    
+    console.log(ctx.request.body);
+
+  })
 
 
 module.exports = router.routes();
