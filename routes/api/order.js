3a8c854aad544e33cab2a869370cc63f3194275e
YYC0626 <yaoyuchen0626@163.com>
2021-04-15 15:59:38 +0800

    a

--- routes/api/order.js
+++ routes/api/order.js
@@ -5,13 +5,73 @@ const db = require('../../module/db');
 
 
 
+
 //显示信息
 router.get('/', async (ctx)=>{
 
-  ctx.body = 'hello api';
+  let id = ctx.request.body.openid;
+  var result = await db.find('user', { "openid" : id});
+  ctx.body = result;
 
 })
 
+
+//add
+router.post('/doAdd', async (ctx)=>{
+  //获取表单数据
+  let data = await db.insert('user', ctx.request.body);
+
+  console.log(data);
+
+})
+
+    
+
+//信息修改
+router.get('/edit', async (ctx)=>{
+
+//  let data = await db.update('user', {"username":"zhangsan"}, {"password":"abcdef"});
+//  console.log(data);
+
+  //通过id获取用户
+  let id = ctx.query.id;
+  let data = await db.find('user', {"_id":db.getObjectID(id)});
+
+ 
+
+
+})
+
+router.post('/doEdit', async (ctx)=>{
+
+  var id = ctx.request.body.id;
+  var username = ctx.request.body.username;
+
+
+  let data = await db.update('user', {"_id":db.getObjectID(id)}, {
+    username, password
+  } )
+
+
+
+})
+
+
+
+//删除
+router.get('/delete', async (ctx)=>{
+//  let data = await db.remove('user', {"username":"zhangsan"});
+//  console.log(data);
+
+  let id = ctx.query.id;
+
+  var data = await db.remove('user', {"_id":db.getObjectID(id)});
+
+
+  
+})
+
+//
 
 
 
